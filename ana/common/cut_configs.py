d141d64f520180b52b90bbb88fee0ae5ab858add
sam-grant <samuel.grant94@gmail.com>
2025-07-03 18:58:35 +0000

    Restructured cut scan analysis to run in detached state

---
+++ ana/common/cut_configs.py
@@ -0,0 +1,10 @@
+cut_configs = [
+    {"nominal": None}, 
+    {"within_lhr_max": False}, 
+    {"within_d0": False},  
+    {"within_pitch_angle": False},  
+    {"within_lhr_max": False, "within_pitch_angle" : False}, 
+    {"within_d0": False, "within_lhr_max": False, "within_pitch_angle" : False} # , 
+    # {"within_pitch_angle": False, "within_pitch_angle_loose": True},  # within_pitch_angle OFF, within_pitch_angle_loose ON
+    # {"within_lhr_max": False, "within_lhr_max_loose": True}  # within_lhr_max OFF, within_lhr_max_loose ON
+]
